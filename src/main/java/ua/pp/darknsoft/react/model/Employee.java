934dfe2c11a29e420ff9a83511faa076dfb3fbd3
andrewdark <samsonov.a@ukr.net>
2023-09-14 11:02:15 +0000

    change username

--- src/main/java/ua/pp/darknsoft/react/model/Employee.java
+++ src/main/java/ua/pp/darknsoft/react/model/Employee.java
@@ -1,4 +1,60 @@
 package ua.pp.darknsoft.react.model;
 
 public class Employee {
+    private String firstName;
+    private String lastName;
+    private String description;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String description) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.description = description;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (!firstName.equals(employee.firstName)) return false;
+        if (!lastName.equals(employee.lastName)) return false;
+        return description != null ? description.equals(employee.description) : employee.description == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
 }
